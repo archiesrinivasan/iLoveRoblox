c3fcc741c3bde935550d2a20c448766b19eaf2c6
Archita Srinivasan <archiesrinivasan0728@gmail.com>
2023-06-26 15:24:50 -0400

    Initial Commit

---
+++ iLoveRoblox/iLoveRobloxApp.swift
@@ -0,0 +1,17 @@
+//
+//  iLoveRobloxApp.swift
+//  iLoveRoblox
+//
+//  Created by scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct iLoveRobloxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
